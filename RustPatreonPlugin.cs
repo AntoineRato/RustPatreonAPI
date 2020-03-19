99df1bd0f81bdca59a9e02877722305ff92d5121
Antoine <antoine.rato@gmail.com>
2020-03-19 20:09:35 +0100

    Adaptation to Covalence Plugin
    • Chatcommand modified to command
    • Trying Player.Message instead Player.Reply to send a message to the player in Rust

--- RustPatreonPlugin.cs
+++ RustPatreonPlugin.cs
@@ -29,44 +29,86 @@ namespace Oxide.Plugins
         }
 
         //#region Commands
-        [ChatCommand("patreon")]
+        [Command("patreon")]
         public void PatreonCommand(IPlayer player, string command, string[] args)
         {
             //if (args.Length < 1)
 
             if (String.Equals(args[0].ToLower(), "claim"))
             {
+                Puts("Command send : " + command);
+                Puts("Arguments : " + args[0].ToLower());
+                Puts("Command recognized : claim");
+
                 player.Reply("Command send : " + command);
                 player.Reply("Arguments : " + args[0].ToLower());
                 player.Reply("Command recognized : claim");
+
+                player.Message("Message : \nCommand send : " + command);
+                player.Message("Arguments : " + args[0].ToLower());
+                player.Message("Command recognized : claim");
             }
-            else if(String.Equals(args[0].ToLower(), "unlink"))
+            else if (String.Equals(args[0].ToLower(), "unlink"))
             {
+                Puts("Command send : " + command);
+                Puts("Arguments : " + args[0].ToLower());
+                Puts("Command recognized : unlink");
+
                 player.Reply("Command send : " + command);
                 player.Reply("Arguments : " + args[0].ToLower());
                 player.Reply("Command recognized : unlink");
+
+                player.Message("Message : \nCommand send : " + command);
+                player.Message("Arguments : " + args[0].ToLower());
+                player.Message("Command recognized : unlink");
             }
             else if (String.Equals(args[0].ToLower(), "link"))
             {
+                Puts("Command send : " + command);
+                Puts("Arguments : " + args[0].ToLower());
+                Puts("Command recognized : link");
+
                 player.Reply("Command send : " + command);
                 player.Reply("Arguments : " + args[0].ToLower());
                 player.Reply("Command recognized : link");
+
+                player.Message("Message : \nCommand send : " + command);
+                player.Message("Arguments : " + args[0].ToLower());
+                player.Message("Command recognized : link");
             }
             else if (String.Equals(args[0].ToLower(), "update"))
             {
+                Puts("Command send : " + command);
+                Puts("Arguments : " + args[0].ToLower());
+                Puts("Command recognized : update");
+
                 player.Reply("Command send : " + command);
                 player.Reply("Arguments : " + args[0].ToLower());
                 player.Reply("Command recognized : update");
+
+                player.Message("Message : \nCommand send : " + command);
+                player.Message("Arguments : " + args[0].ToLower());
+                player.Message("Command recognized : update");
             }
             else if (String.Equals(args[0].ToLower(), "check"))
             {
+                Puts("Command send : " + command);
+                Puts("Arguments : " + args[0].ToLower());
+                Puts("Command recognized : check");
+
                 player.Reply("Command send : " + command);
                 player.Reply("Arguments : " + args[0].ToLower());
                 player.Reply("Command recognized : check");
+
+                player.Message("Message : \nCommand send : " + command);
+                player.Message("Arguments : " + args[0].ToLower());
+                player.Message("Command recognized : check");
             }
             else
             {
+                Puts("Unrecognized command, please retry.");
                 player.Reply("Unrecognized command, please retry.");
+                player.Message("Message : \nUnrecognized command, please retry.");
             }
         }
     }
